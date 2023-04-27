bff2fbd52bde45f310d9ab29a95abb46d2f68659
papasnowee <gosusnow@yandex.ru>
2023-04-26 17:28:01 -0700

    added the English version of timeAgo function

--- src/components/Comments/utils.ts
+++ src/components/Comments/utils.ts
@@ -9,56 +9,36 @@ export const sortByDate = (arr: { created: string }[]) => {
     })
 }
 
-// example: if 1 hour left since a comment has been sent, it should return '1 час назад'
-export const calcPassedTime = (time: Date) => {
-    const hours = (+new Date() - +time) / 1000 / 3600
-    if (hours >= 1 && hours < 24) {
-        if (hours === 1 || hours === 21) return `${hours} час назад`
-        if (hours === 2 || hours === 3 || hours === 4 || hours === 22 || hours === 23) return `${hours} часа назад`
-        return `${hours} часов назад`
-    }
-}
-
 export function timeAgo(date: Date) {
-    const minute = 60 * 1000
-    const hour = minute * 60
-    const day = hour * 24
+    const seconds = Math.floor((Number(new Date()) - Number(date)) / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
 
-    const timeElapsed = Date.now() - date.getTime()
-
-    if (timeElapsed < minute) {
-        return 'только что'
-    } else if (timeElapsed < hour) {
-        const minutesAgo = Math.round(timeElapsed / minute)
-        return `${minutesAgo} ${declOfNum(minutesAgo, ['минуту', 'минуты', 'минут'])} назад`
-    } else if (timeElapsed < day) {
-        const hoursAgo = Math.round(timeElapsed / hour)
-        return `${hoursAgo} ${declOfNum(hoursAgo, ['час', 'часа', 'часов'])} назад`
+    if (seconds < 60) {
+        return 'recently'
+    } else if (seconds < 120) {
+        return '1 second ago'
+    } else if (seconds < 300) {
+        return `${Math.floor(seconds / 60)} seconds ago`
+    } else if (seconds < 3600) {
+        return `${minutes} minutes ago`
+    } else if (seconds < 7200) {
+        return '1 hour ago'
+    } else if (seconds < 86400) {
+        return `${hours} hours ago`
     } else {
-        const dateFormat = new Intl.DateTimeFormat('ru-RU', {
-            day: 'numeric',
-            month: 'numeric',
+        // If more than a day has passed, return the date and time in US format
+        const dateFormat = new Intl.DateTimeFormat('en-US', {
+            weekday: 'long',
             year: 'numeric',
+            month: 'long',
+            day: 'numeric',
             hour: 'numeric',
             minute: 'numeric',
-            second: 'numeric',
+            timeZone: 'America/New_York',
         })
         return dateFormat.format(date)
     }
-}
-
-// Функция declOfNum возвращает нужную форму слова в зависимости от числа
-function declOfNum(number: number, titles: string[]) {
-    const cases = [2, 0, 1, 1, 1, 2]
-    return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]]
-}
-
-// копирует массив объектов
-export function createObjectList<P>(objectList: P[]) {
-    const commentsWithChildren: P[] = []
-    objectList.forEach((item) => commentsWithChildren.push(Object.assign({}, item)))
-
-    return commentsWithChildren
 }
 
 // копирует элементы детей в объекты родителей
@@ -74,10 +54,4 @@ export function moveChildrenToParent(arr: IComment[]) {
             }
         }
     })
-}
-
-export const calcLikes = (arr: IComment[]) => {
-    return arr.reduce((acc, current) => {
-        return acc + current.likes
-    }, 0)
 }
